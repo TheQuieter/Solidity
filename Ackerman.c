32d654b2fbe4af65c985003f323d3ba38da00741
Rajesh Prajapati <rajesh408mg@gmail.com>
2018-08-02 08:05:47 +0530

    Contracts, Functions, State Variables, Getters & Setters

---
+++ Ackerman.c
@@ -0,0 +1,17 @@
+pragma solidity ^0.4.11;
+contract GetterSetter {
+    string name;
+    uint age;
+    function getName() view returns (string) {
+        return name;
+    }
+    function setName(string Name){
+        name = Name;
+    }
+    function getAge() view returns (uint) {
+        return age;
+    }
+    function setAge(uint Age){
+        age = Age;
+    }
+}
