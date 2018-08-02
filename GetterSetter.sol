4302084641be20f5f5e1be3d0246246764e50a30
Rajesh Prajapati <rajesh408mg@gmail.com>
2018-08-02 08:07:42 +0530

    Update and rename Ackerman.c to GetterSetter.sol

--- GetterSetter.sol
+++ GetterSetter.sol
@@ -1,16 +1,21 @@
 pragma solidity ^0.4.11;
+
 contract GetterSetter {
     string name;
     uint age;
+
     function getName() view returns (string) {
         return name;
     }
-    function setName(string Name){
-        name = Name;
-    }
+    
     function getAge() view returns (uint) {
         return age;
     }
+    
+    function setName(string Name){
+        name = Name;
+    }
+    
     function setAge(uint Age){
         age = Age;
     }
